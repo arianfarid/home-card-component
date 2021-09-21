4164e382c6a74477a23b5e1167430a34f74ae255
arianfarid <arian.blank.farid@gmail.com>
2021-09-21 17:09:32 -0400

    Update HouseCard.vue

--- src/components/HouseCard.vue
+++ src/components/HouseCard.vue
@@ -2,7 +2,7 @@
     <div class="w-full bg-white sm:w-80 sm:rounded-lg sm:shadow-lg sm:m-2 lg:w-96">
         <!-- images and favorite -->
         <div class="h-48 bg-gray-100 sm:rounded-t-lg relative">
-            <!-- favorite icon -->
+            <!-- favorite icon from: https://heroicons.dev/ -->
             <div v-if="!favorite" class="absolute top-0 right-0 z-10 p-1" v-on:click="emitToggleFavorite()">
                 <svg class="w-6 h-6" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
